b62f6d7ee5ed2a2fbe1a1bd130d48af2b14f0f0c
王靖 <445091571@qq.com>
2019-02-02 10:47:47 +0800

    添加注释

--- eureka_feign/src/main/java/com/eurekafeign/EurakeFeginApplication.java
+++ eureka_feign/src/main/java/com/eurekafeign/EurakeFeginApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-
+//
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
