90e5945f5929000391fdb88e05afcf5702eb3475
王靖 <445091571@qq.com>
2019-02-02 10:41:50 +0800

    首次提交

---
+++ eureka_feign/src/main/java/jwang/interfac/TestInterface.java
@@ -0,0 +1,12 @@
+package jwang.interfac;
+/**
+ * 远程服务调用类，name与服务名相同
+ */
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name= "eureka-client")
+public interface TestInterface {
+	@GetMapping(value = "/get")
+    String get();
+}
